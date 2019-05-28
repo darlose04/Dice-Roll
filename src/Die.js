7df0993ba0a20f8fb698f9d1a95875e054f5d520
Zach Smith <zsmith126028@gmail.com>
2019-05-28 18:49:26 -0500

    add Die component with initial test element

---
+++ src/Die.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Die extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is the die component.</p>
+      </div>
+    )
+  }
+}
+
+export default Die;
