08fe044a7a435f12ba497abca66b59afa509acf3
Zach Smith <zsmith126028@gmail.com>
2019-05-28 20:09:38 -0500

    add state to the two dice

--- src/RollDice.js
+++ src/RollDice.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import Die from './Die';
 
 class RollDice extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      die1: 'one',
+      die2: 'one'
+    }
+  }
   render() {
     return (
       <div>
-        <Die />
-        <Die />
-        <button>Roll Dice!</button>
+        <Die face={this.state.die1} />
+        <Die face={this.state.die2} />
+        
+        
+        {/* <button>Roll Dice!</button> */}
       </div>
     )
   }
