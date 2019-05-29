2875a53a4120315636187a10c83677ebd93ce136
Zach Smith <zsmith126028@gmail.com>
2019-05-28 20:15:49 -0500

    add roll function to onClick

--- src/RollDice.js
+++ src/RollDice.js
@@ -5,13 +5,27 @@ class RollDice extends Component {
   static defaultProps = {
     sides: ['one', 'two', 'three', 'four', 'five', 'six']
   }
-  
+
   constructor(props) {
     super(props);
     this.state = {
       die1: 'one',
       die2: 'one'
     }
+    this.roll = this.roll.bind(this)
+  }
+
+  roll() {
+    // pick two new rolls 
+    const newDie1 = this.props.sides[
+      Math.floor(Math.random() * this.props.sides.length)
+    ];
+    const newDie2 = this.props.sides[
+      Math.floor(Math.random() * this.props.sides.length)
+    ];
+
+    // set state with new rolls
+    this.setState({ die1: newDie1, die2: newDie2 });
   }
 
   render() {
@@ -19,8 +33,7 @@ class RollDice extends Component {
       <div>
         <Die face={this.state.die1} />
         <Die face={this.state.die2} />
-        
-        {/* <button>Roll Dice!</button> */}
+        <button onClick={this.roll}>Roll Dice!</button>
       </div>
     )
   }
